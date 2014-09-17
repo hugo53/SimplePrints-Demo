499009b13a9ccba4c715256acfd5769e5e8a78d1
Hoang Nguyen <hoang@Hoangs-MacBook-Pro-2.local>
2014-09-16 19:15:57 -0700

    Initial Commit

---
+++ SimplePrints-Demo/HUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HUAppDelegate.h
+//  SimplePrints-Demo
+//
+//  Created by Hoang on 9/16/14.
+//  Copyright (c) 2014 StoryTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimplePrints-Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimplePrints-Demo
+//
+//  Created by Hoang on 9/16/14.
+//  Copyright (c) 2014 StoryTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HUAppDelegate class]));
+    }
+}
